2beb5ac7fff27e130fdc299a00e9f6aa49378f53
Reka Kulinyi <reekaa85@gmail.com>
2018-03-12 21:16:36 +0100

    seeds updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,29 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
+Price.destroy_all
 Stock.destroy_all
 
 p "calling api"
 
 stocks = StockQuote::Stock.quote("aapl,amzn,msft,tsla")
 
-p "creating stocks"
+p "creating stocks.."
 
-stocks.each do |st|
-  stock = Stock.new(ticker: st.t, name: st.name, price: st.l * 100)
-  stock.save!
+stocks.each do |stock|
+  new_stock = Stock.new(ticker: stock.t, name: stock.name)
+  new_stock.save!
 end
 
 p "stocks created"
+
+all_stocks = Stock.all
+
+p "creating prices.."
+
+stocks.each_with_index do |stock, index|
+  new_price = Price.new(price: stock.l, growth: stock.c, date: Date.today, stock: all_stocks[index])
+  new_price.save!
+end
+
+p "prices created"
