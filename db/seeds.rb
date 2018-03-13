344bf9bc761a7489d153ede7f94e3474e49a14ec
Reka Kulinyi <reekaa85@gmail.com>
2018-03-13 21:47:57 +0100

    5 days stock history added to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,7 @@ p "calling api"
 
 stocks = StockQuote::Stock.quote("aapl,amzn,msft,tsla")
 
+
 p "creating stocks.."
 
 stocks.each do |stock|
@@ -19,11 +20,34 @@ p "stocks created"
 
 all_stocks = Stock.all
 
-p "creating prices.."
+p "creating today's prices.."
 
 stocks.each_with_index do |stock, index|
   new_price = Price.new(price: stock.l, growth: stock.c, date: Date.today, stock: all_stocks[index])
   new_price.save!
+end
+
+p "creating last 5 day's prices.."
+
+stock_symbols = ["aapl","amzn","msft","tsla"]
+
+
+last_5_days_all_stocks = []
+
+# we request the 5 day history of each stock (no stock market on weekends and holidays!)
+# the data sent back (stock_history) is a hash like: {:symbol=>"amzn", :history=>[{:date=>"12-Mar-18", :open=>1592.6,...
+# it does not send back growth
+stock_symbols.each do |symbol|
+  stock_history = StockQuote::Stock.history(symbol, 5.days.ago, Date.today)
+  last_5_days_all_stocks << stock_history
+end
+
+last_5_days_all_stocks.each do |stock|
+  stock_name = Stock.find_by(ticker: stock[:symbol].upcase)
+  stock[:history].each do |day|
+    price = Price.new(price: day[:close], date: Date.parse(day[:date]), stock: stock_name)
+    price.save!
+  end
 end
 
 p "prices created"
--- spec/models/price_spec.rb
+++ spec/models/price_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe Price, type: :model do
   end
 
   it { is_expected.to validate_presence_of(:price) }
-  it { is_expected.to validate_presence_of(:growth) }
+  #it { is_expected.to validate_presence_of(:growth) }
   it { is_expected.to validate_presence_of(:date) }
   it { is_expected.to belong_to(:stock) }
 end
