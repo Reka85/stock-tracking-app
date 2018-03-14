4f0d1899c740d906fec04453fff664f4d2da1657
Reka Kulinyi <reekaa85@gmail.com>
2018-03-14 21:25:31 +0100

    graph for prices

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,8 @@
 // about supported directives.
 //
 //= require jquery
-//= require rails-ujs
+//= require jquery_ujs
 //= require bootstrap-sprockets
 //= require d3
-//= require turbolinks
+// require turbolinks
 //= require_tree .
